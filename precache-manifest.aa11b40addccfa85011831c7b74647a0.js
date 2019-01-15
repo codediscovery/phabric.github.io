33393c30f610b1bfb62e1821ef16035e619def8f
axonian <trigub_nikita@i.ua>
2019-01-15 22:53:33 +0200

    initial-commit

---
+++ precache-manifest.aa11b40addccfa85011831c7b74647a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08af59a3e98c3fd05d60",
+    "url": "/static/js/main.08af59a3.chunk.js"
+  },
+  {
+    "revision": "8f5193b9fa03b8ec9550",
+    "url": "/static/js/1.8f5193b9.chunk.js"
+  },
+  {
+    "revision": "ea7d69556c400474178bf15d34d54859",
+    "url": "/index.html"
+  }
+];
